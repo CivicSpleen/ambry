ba7274661bbc6e22245e8c54e6275ffc3e880d98
Eric Busboom <eric@clarinova.com>
2014-04-09 12:56:42 -0700

    still testing web hooks

--- _ipython/convert.py
+++ _ipython/convert.py
@@ -13,23 +13,41 @@ def modification_date(filename):
     return datetime.datetime.fromtimestamp(t)
 
 parser = argparse.ArgumentParser(description='Convert an ipython notebook to  HTML')
-parser.add_argument('file', metavar='file',  type=str,  help='The file to convert')
+parser.add_argument('files', metavar='files', nargs='+',  type=str,  help='The file to convert')
 
 args = parser.parse_args()
 
-root_dir =  os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+def convert(exportHTML, nb_file):
 
-nb_file = args.file
-nb_md = modification_date(nb_file)
+   
+    nb_md = modification_date(nb_file)
 
-out_file = nb_file.replace('.ipynb','.html')
+    out_file = nb_file.replace('.ipynb','.html')
 
-if os.path.exists(out_file):
-    out_md = modification_date(out_file)
+    if os.path.exists(out_file):
+        out_md = modification_date(out_file)
 
-    if out_md > nb_md:
-        sys.exit(0) 
+        if out_md > nb_md:
+            return None
 
+
+    #print "NBConvert: {}".format(nb_file)
+
+    with open(nb_file) as f:
+        notebook = nbformat.reads_json(f.read())
+
+    ## I use basic here to have less boilerplate and headers in the HTML.
+    ## we'll see later how to pass config to exporters.
+
+
+    (body,resources) = exportHtml.from_notebook_node(notebook)
+
+
+    with open(out_file,'w') as f:
+        f.write(body.encode('utf8'))
+
+    return out_file
+    
 config=Config(
 {
     'HTMLExporter':{
@@ -39,17 +57,16 @@ config=Config(
 }
 )
 
-with open(nb_file) as f:
-    notebook = nbformat.reads_json(f.read())
-
-## I use basic here to have less boilerplate and headers in the HTML.
-## we'll see later how to pass config to exporters.
 exportHtml = HTMLExporter(config=config)
 
-(body,resources) = exportHtml.from_notebook_node(notebook)
-
-
-with open(out_file,'w') as f:
-    f.write(body.encode('utf8'))
-
-print  out_file
+out_files = []
+for file in args.files:
+    
+    out_file = convert(exportHtml, file)
+    
+    if out_file:
+        out_files.append(out_file)
+    
+    
+print '\n'.join(out_files)
+    
