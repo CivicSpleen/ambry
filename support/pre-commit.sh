3a4e96a7d1ede5322cbb9fd20f21e2326637c668
Eric Busboom <eric@clarinova.com>
2014-04-13 21:29:35 -0700

    .

--- support/pre-commit.sh
+++ support/pre-commit.sh
@@ -7,4 +7,7 @@ COUNT=$(git rev-list HEAD --count)
 cat $DIR/ambry/__init__.py | \
 perl -pe "s/^__version__\s*=\s*'(\d+)\.(\d+).*/__version__ = '\1.\2.$COUNT'/" > $DIR/ambry/__init__.py.new
 
+
 mv $DIR/ambry/__init__.py.new $DIR/ambry/__init__.py
+git add $DIR/ambry/__init__.py
+
