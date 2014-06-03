85482b636da3c0cb1eb356390ac3df08a5ee7d81
Eric Busboom <eric@clarinova.com>
2014-06-02 22:01:40 -0700

    Hacking install process

--- support/install-ubuntu.sh
+++ support/install-ubuntu.sh
@@ -27,7 +27,7 @@ sudo pip install git+https://github.com/clarinova/pysqlite.git#egg=pysqlite
 ### Install Ambry
 ###
 
-sudo pip install -I ambry
+sudo pip install ambry
 
 ambry config install # Installs a development config
 
