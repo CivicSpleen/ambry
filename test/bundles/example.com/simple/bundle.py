7806657b06011872285223169e74cfb2f1afb59a
Eric Busboom <eric@clarinova.com>
2014-10-12 21:20:22 -0700

    changing bundle names

---
+++ test/bundles/example.com/simple/bundle.py
@@ -0,0 +1,37 @@
+'''
+'''
+
+from  ambry.bundle import BuildBundle
+ 
+
+class Bundle(BuildBundle):
+    ''' '''
+ 
+    def __init__(self,directory=None):
+        
+        super(Bundle, self).__init__(directory)
+ 
+    def build(self):
+        import uuid
+        import random
+
+        p = self.partitions.new_partition(table='example')
+        
+        p.query('DELETE FROM example')
+        nd = p.table.null_dict
+        
+        lr = self.init_log_rate(print_rate=5)
+        
+        with p.database.inserter() as ins:
+            for i in range(100000):
+                row = dict(nd.items())
+            
+                row['uuid'] = str(uuid.uuid4())
+                row['int'] = random.randint(0,100)
+                row['float'] = random.random()*100
+        
+                ins.insert(row)
+                lr()
+        
+        return True
+
