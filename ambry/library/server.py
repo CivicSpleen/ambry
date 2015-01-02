5f471e5a7a40830e2bd6b645546f7ffffcee2ee9
Eric Busboom <eric@clarinova.com>
2015-01-01 19:27:29 -0800

    .

--- ambry/library/server.py
+++ ambry/library/server.py
@@ -17,77 +17,76 @@ from ambrydoc import fscache
 
 app.register_blueprint(exracts_blueprint)
 
-parser = argparse.ArgumentParser(prog='python -mambry.server.documentation',
-                                 description='Run an Ambry documentation server')
+if __name__ == "__main__":
 
-parser.add_argument('-I', '--install', action='store_true', help="Install configuration file and exit")
-parser.add_argument('-H', '--host', help="Server host.")
-parser.add_argument('-p', '--port', help="Server port")
-parser.add_argument('-c', '--cache', help="Generated file cache. ")
-parser.add_argument('-P', '--use-proxy', action='store_true',
-                    help="Setup for using a proxy in front of server, using werkzeug.contrib.fixers.ProxyFix")
-parser.add_argument('-d', '--debug',  action='store_true',  help="Set debugging mode")
-parser.add_argument('-C', '--check-config',  action='store_true',  help="print cache string and configuration file and exit")
-parser.add_argument('-i', '--index',  action='store_true',  help="Update the search index")
-parser.add_argument('-r', '--reindex',  action='store_true',  help="Recreate the search index")
+    parser = argparse.ArgumentParser(prog='python -mambry.server.documentation',
+                                     description='Run an Ambry documentation server')
 
-parser.add_argument('-s', '--search',  help="search for a term in the search index")
+    parser.add_argument('-I', '--install', action='store_true', help="Install configuration file and exit")
+    parser.add_argument('-H', '--host', help="Server host.")
+    parser.add_argument('-p', '--port', help="Server port")
+    parser.add_argument('-c', '--cache', help="Generated file cache. ")
+    parser.add_argument('-P', '--use-proxy', action='store_true',
+                        help="Setup for using a proxy in front of server, using werkzeug.contrib.fixers.ProxyFix")
+    parser.add_argument('-d', '--debug',  action='store_true',  help="Set debugging mode")
+    parser.add_argument('-C', '--check-config',  action='store_true',  help="print cache string and configuration file and exit")
+    parser.add_argument('-i', '--index',  action='store_true',  help="Update the search index")
+    parser.add_argument('-r', '--reindex',  action='store_true',  help="Recreate the search index")
 
-args = parser.parse_args()
+    parser.add_argument('-s', '--search',  help="search for a term in the search index")
 
-config = configure_application(vars(args))
+    args = parser.parse_args()
 
-if args.index or args.reindex:
-    from ambrydoc.search import Search
-    from ambrydoc.cache import DocCache
+    config = configure_application(vars(args))
 
-    print 'Updating the search index'
+    if args.index or args.reindex:
+        from ambrydoc.search import Search
+        from ambrydoc.cache import DocCache
 
-    s = Search(DocCache(fscache()))
+        print 'Updating the search index'
 
-    s.index(reset=args.reindex)
+        s = Search(DocCache(fscache()))
 
-    sys.exit(0)
+        s.index(reset=args.reindex)
 
-if args.search:
-    from ambrydoc.search import Search
-    from ambrydoc.cache import DocCache
+        sys.exit(0)
 
-    print "Search for ", args.search
+    if args.search:
+        from ambrydoc.search import Search
+        from ambrydoc.cache import DocCache
 
-    s = Search(DocCache(fscache()))
+        print "Search for ", args.search
 
-    for r in s.search(args.search):
-        print r
+        s = Search(DocCache(fscache()))
 
-    sys.exit(0)
+        for r in s.search(args.search):
+            print r
 
-if args.install:
-    import yaml
-    from ambrydoc import app_config
+        sys.exit(0)
 
-    path = write_config(app_config,config_paths)
+    if args.install:
+        import yaml
+        from ambrydoc import app_config
 
-    print app_config
+        path = write_config(app_config,config_paths)
 
-    print "Wrote default config to ", path
+        print app_config
 
-    sys.exit(0)
+        print "Wrote default config to ", path
 
-if args.use_proxy:
-    from werkzeug.contrib.fixers import ProxyFix
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+        sys.exit(0)
 
-
-if args.check_config:
-
-    import yaml
-
-    print "Using cache: ", fscache()
-
-    sys.exit(0)
+    if args.use_proxy:
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
-print fscache()
+    if args.check_config:
 
-app.run(host = config['host'], port = int(config['port']), debug = config['debug'])
+        import yaml
+
+        print "Using cache: ", fscache()
+
+        sys.exit(0)
+
+    app.run(host = config['host'], port = int(config['port']), debug = config['debug'])
