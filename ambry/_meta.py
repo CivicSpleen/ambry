4c0f0f9c645fa31d15c3e2f0fcaa3b51dbb1eec5
Eric Busboom <eric@clarinova.com>
2015-04-20 14:52:28 -0700

    update version

--- ambry/_meta.py
+++ ambry/_meta.py
@@ -1,10 +1,10 @@
 """Package metadata."""
 
 __author__ = "Eric Busboom"
-__copyright__ = "Copyright (c) 2014 Clarinova"
+__copyright__ = "Copyright (c) 2015 Civic Knowledge"
 __credits__ = []
 __license__ = "Revised BSD"
-__version__ = '0.3.612'
+__version__ = '0.3.613'
 __maintainer__ = "Eric Busboom"
-__email__ = "eric@clarinova.com"
+__email__ = "eric@civicknowledge.com"
 __status__ = "Development"
