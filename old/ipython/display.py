b9edd37388bc7914fe027d26557889710202e83d
Eric Busboom <eric@clarinova.com>
2015-04-22 07:54:45 -0700

    Moved old code

---
+++ old/ipython/display.py
@@ -0,0 +1,14 @@
+
+
+def display_list(l, list_type='ul'):
+    """Convert a list of strings into an HTML list."""
+    from IPython.core.display import display_html
+
+    o = ["<{}>".format(list_type)]
+
+    for i in l:
+        o.append("<li>{}</li>".format(str(i)))
+
+    o.append("</{}>".format(list_type))
+
+    return display_html('\n'.join(o), raw=True)
