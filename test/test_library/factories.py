b7f2a64f6988fc64f13329b2baaa6ca714a4596f
Kazbek <nmb.ten@gmail.com>
2015-05-02 10:41:38 +0300

    library.database tests added. #CivicKnowledge/ambry#8.

---
+++ test/test_library/factories.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+import factory
+from factory.alchemy import SQLAlchemyModelFactory
+
+from ambry.orm import Dataset, Config, Table, Column, File, Partition, Code, ColumnStat
+
+# TODO: move to the orm tests dir
+
+
+class DatasetFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Dataset
+        sqlalchemy_session = None  # Set that just before ConfigFactory using.
+
+    vid = factory.Sequence(lambda n: '%03d' % n)
+    name = factory.Sequence(lambda n: 'name-%03d' % n)
+    vname = factory.Sequence(lambda n: 'vname-%03d' % n)
+    fqname = factory.Sequence(lambda n: 'fqname-%03d' % n)
+    cache_key = factory.Sequence(lambda n: 'cache_key-%03d' % n)
+    source = factory.Sequence(lambda n: 'source-%03d' % n)
+    dataset = factory.Sequence(lambda n: 'dataset-%03d' % n)
+    creator = factory.Sequence(lambda n: 'creator-%03d' % n)
+    revision = 1
+    version = '0.1'
+
+
+class ConfigFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Config
+        sqlalchemy_session = None  # Set that just before ConfigFactory using.
+
+    d_vid = factory.Sequence(lambda n: '%03d' % n)
+    group = factory.Sequence(lambda n: '%03d' % n)
+
+
+class TableFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Table
+        sqlalchemy_session = None  # Set that just before TableFactory using.
+
+    vid = factory.Sequence(lambda n: '%03d' % n)
+    sequence_id = 1
+    name = factory.Sequence(lambda n: 'name-%03d' % n)
+    data = {}
+
+
+class ColumnFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Column
+        sqlalchemy_session = None  # Set that just before TableFactory using.
+
+    vid = factory.Sequence(lambda n: '%03d' % n)
+
+
+class FileFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = File
+        sqlalchemy_session = None  # Set that just before TableFactory using.
+
+    oid = factory.Sequence(lambda n: n)
+    path = factory.Sequence(lambda n: 'path-%03d' % n)
+
+
+class PartitionFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Partition
+        sqlalchemy_session = None  # Set that just before TableFactory using.
+
+    vid = factory.Sequence(lambda n: 'vid-%03d' % n)
+    id_ = factory.Sequence(lambda n: 'id_-%03d' % n)
+    name = factory.Sequence(lambda n: 'name-%03d' % n)
+    vname = factory.Sequence(lambda n: 'vname-%03d' % n)
+    fqname = factory.Sequence(lambda n: 'fqname-%03d' % n)
+    format = 'csv'
+    cache_key = factory.Sequence(lambda n: 'cache_key-%03d.csv' % n)
+
+
+class CodeFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = Code
+        sqlalchemy_session = None  # Set that just before CodeFactory using.
+    oid = factory.Sequence(lambda n: n)
+    key = factory.Sequence(lambda n: 'key-%03d' % n)
+    value = factory.Sequence(lambda n: 'value-%03d' % n)
+
+
+class ColumnStatFactory(SQLAlchemyModelFactory):
+    class Meta:
+        model = ColumnStat
+        sqlalchemy_session = None  # Set that just before TableFactory using.
+
+    id = factory.Sequence(lambda n: n)
---
+++ test/test_library/test_database.py
@@ -0,0 +1,223 @@
+import os
+import unittest
+
+import fudge
+
+from ambry.library.database import LibraryDb
+
+from test.test_library.factories import DatasetFactory, ConfigFactory,\
+    TableFactory, ColumnFactory, FileFactory, PartitionFactory, CodeFactory,\
+    ColumnStatFactory
+
+
+class LibraryDbTest(unittest.TestCase):
+    def setUp(self):
+        self.sqlite_db = LibraryDb(driver='sqlite', dbname='test_database.db')
+        self.sqlite_db.enable_delete = True
+
+    def tearDown(self):
+        self.sqlite_db.drop()
+        os.remove('test_database.db')
+
+    def test_initialization_raises_exception_if_driver_not_found(self):
+        with self.assertRaises(ValueError):
+            LibraryDb(driver='1')
+
+    def test_initialization_populates_port(self):
+        db = LibraryDb(driver='postgres', port=5232)
+        self.assertIn('5232', db.dsn)
+
+    def test_initialization_uses_library_schema_for_postgres(self):
+        db = LibraryDb(driver='postgres')
+        self.assertEquals(db._schema, 'library')
+
+    def test_initialization_uses_library_schema_for_postgis(self):
+        db = LibraryDb(driver='postgis')
+        self.assertEquals(db._schema, 'library')
+
+    @fudge.patch(
+        'sqlalchemy.create_engine')
+    def test_engine_creates_new_sqlalchemy_engine(self, fake_create):
+        engine_stub = fudge.Fake().is_a_stub()
+        fake_create.expects_call()\
+            .returns(engine_stub)
+        db = LibraryDb(driver='postgis')
+        self.assertEquals(db.engine, engine_stub)
+
+    @fudge.patch(
+        'sqlalchemy.create_engine',
+        'sqlalchemy.event',
+        'ambry.database.sqlite._on_connect_update_sqlite_schema')
+    def test_engine_listens_to_connect_signal_for_sqlite_driver(self, fake_create,
+                                                                fake_event, fake_on):
+        fake_event\
+            .provides('listen')
+        engine_stub = fudge.Fake().is_a_stub()
+        fake_create.expects_call()\
+            .returns(engine_stub)
+        fake_on.expects_call()
+        db = LibraryDb(driver='sqlite')
+        self.assertEquals(db.engine, engine_stub)
+
+    def test_connection_creates_new_sqlalchemy_connection(self):
+        fake_connection = fudge.Fake()
+
+        fake_engine = fudge.Fake()\
+            .provides('connect')\
+            .returns(fake_connection)
+
+        db = LibraryDb(driver='sqlite')
+        db._engine = fake_engine
+        self.assertEquals(db.connection, fake_connection)
+
+    def test_connection_sets_path_to_library_for_postgres(self):
+        fake_connection = fudge.Fake('connection')\
+            .provides('execute')\
+            .with_args('SET search_path TO library')\
+            .expects_call()
+
+        fake_engine = fudge.Fake()\
+            .provides('connect')\
+            .returns(fake_connection)
+
+        db = LibraryDb(driver='postgres')
+        db._engine = fake_engine
+        self.assertEquals(db.connection, fake_connection)
+
+    def test_connection_sets_path_to_library_for_postgis(self):
+        fake_connection = fudge.Fake('connection')\
+            .provides('execute')\
+            .with_args('SET search_path TO library')\
+            .expects_call()
+
+        fake_engine = fudge.Fake()\
+            .provides('connect')\
+            .returns(fake_connection)
+
+        db = LibraryDb(driver='sqlite')
+        db._engine = fake_engine
+        self.assertEquals(db.connection, fake_connection)
+
+    def test_commit_commits_session(self):
+        fake_session = fudge.Fake('session')\
+            .provides('commit')\
+            .expects_call()
+        db = LibraryDb(driver='sqlite')
+        db.Session = fake_session
+        db._session = fake_session
+        db.commit()
+
+    def test_commit_raises_session_commit_exception(self):
+        fake_session = fudge.Fake('session')\
+            .provides('commit')\
+            .expects_call()\
+            .raises(ValueError)
+        db = LibraryDb(driver='sqlite')
+        db.Session = fake_session
+        db._session = fake_session
+        with self.assertRaises(ValueError):
+            db.commit()
+
+    # exists tests
+    def test_sqlite_database_does_not_exists_if_file_not_found(self):
+        db = LibraryDb(driver='sqlite', dbname='no-such-file.db')
+        self.assertFalse(db.exists())
+
+    # clone tests
+    def test_clone_returns_new_instance(self):
+        db = LibraryDb(driver='sqlite')
+        new_db = db.clone()
+        self.assertNotEquals(db, new_db)
+        self.assertEquals(db.driver, new_db.driver)
+        self.assertEquals(db.server, new_db.server)
+        self.assertEquals(db.dbname, new_db.dbname)
+        self.assertEquals(db.username, new_db.username)
+        self.assertEquals(db.password, new_db.password)
+
+    # create_tables test
+    def test_creates_dataset_table(self):
+        self.sqlite_db.create_tables()
+
+        # Now all tables are created. Can we use ORM to create datasets?
+        DatasetFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        DatasetFactory()
+        self.sqlite_db.session.commit()
+
+    def test_creates_config_table(self):
+        self.sqlite_db.create_tables()
+
+        # Now all tables are created. Can we use ORM to create configs?
+        ConfigFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        ConfigFactory(key='a', value='b')
+        self.sqlite_db.session.commit()
+
+    def test_creates_table_table(self):
+        self.sqlite_db.create_tables()
+
+        # Now all tables are created. Can we use ORM to create configs?
+        TableFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        DatasetFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        ds1 = DatasetFactory()
+        self.sqlite_db.session.commit()
+        TableFactory(dataset=ds1)
+        self.sqlite_db.session.commit()
+
+    def test_creates_column_table(self):
+        self.sqlite_db.create_tables()
+
+        # Now all tables are created. Can we use ORM to create columns?
+        DatasetFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        TableFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        ColumnFactory._meta.sqlalchemy_session = self.sqlite_db.session
+
+        # it requires table and dataset.
+        ds1 = DatasetFactory()
+        self.sqlite_db.session.commit()
+
+        table1 = TableFactory(dataset=ds1)
+        self.sqlite_db.session.commit()
+
+        ColumnFactory(table=table1)
+        self.sqlite_db.session.commit()
+
+    def test_creates_file_table(self):
+        self.sqlite_db.create_tables()
+        FileFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        FileFactory()
+        self.sqlite_db.session.commit()
+
+    def test_creates_partition_table(self):
+        self.sqlite_db.create_tables()
+        PartitionFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        DatasetFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        ds1 = DatasetFactory()
+        self.sqlite_db.session.commit()
+        PartitionFactory(dataset=ds1)
+        self.sqlite_db.session.commit()
+
+    def test_creates_code_table(self):
+        self.sqlite_db.create_tables()
+        CodeFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        CodeFactory()
+        self.sqlite_db.session.commit()
+
+    def test_creates_columnstat_table(self):
+        self.sqlite_db.create_tables()
+        DatasetFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        TableFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        ColumnFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        PartitionFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        ColumnStatFactory._meta.sqlalchemy_session = self.sqlite_db.session
+
+        ds1 = DatasetFactory()
+        self.sqlite_db.session.commit()
+        partition1 = PartitionFactory(dataset=ds1)
+
+        table1 = TableFactory(dataset=ds1)
+        self.sqlite_db.session.commit()
+
+        column1 = ColumnFactory(table=table1)
+
+        ColumnStatFactory._meta.sqlalchemy_session = self.sqlite_db.session
+        ColumnStatFactory(partition=partition1, column=column1)
+        self.sqlite_db.session.commit()
