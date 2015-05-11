dc2ceca01610a28dc1471606af5292179e8ed7b2
Eric Busboom <eric@clarinova.com>
2015-05-11 13:26:03 -0700

    fixing titles and summaries for old bundles

--- ambry/library/doccache.py
+++ ambry/library/doccache.py
@@ -143,6 +143,9 @@ class DocCache(object):
                     d['title'] =  ds.config('about.title').value
                     d['summary'] = ds.config('about.summary').value
                 except NoResultFound:
+                    print '!!! '
+                    for c in ds.configs:
+                        print '   ', ds.dict
                     pass
 
             return d
