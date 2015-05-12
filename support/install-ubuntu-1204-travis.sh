2f10a5b0b8e8fd323a5bd2a4842414df4d1f6544
Eric Busboom <eric@clarinova.com>
2015-05-12 15:15:31 -0700

    Updated to match the .travis file

--- support/install-ubuntu-1204-travis.sh
+++ support/install-ubuntu-1204-travis.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
-# Install script for Ubuntu 12.04, which is used in Travis CI
+# Install script version of the installtion in the .travis file.
 # apt-get update && apt-get install -y curl && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ericbusboom/ambry/develop/support/install-ubuntu-1204.sh)"
 
 TRAVIS_PYTHON_VERSION=2.7
 travis_retry=
 
-apt-get update
-locale-gen en_US.UTF-8
 
-apt-get install -y git gcc g++ wget bzip2 libpq-dev libspatialindex-dev
-
+sudo apt-get update -qq
+sudo apt-get install -qq wget bzip2 libpq-dev libspatialindex-dev libffi-dev
+sudo apt-get install -qq libspatialite5 libspatialite-dev spatialite-bin libspatialindex-dev libspatialindex-dev
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b
-export PATH=/home/travis/miniconda/bin:/miniconda/bin:$PATH
+export PATH=/home/travis/miniconda/bin:$PATH
 conda update --yes conda
 travis_retry conda install --yes python=$TRAVIS_PYTHON_VERSION pip numpy scipy
-travis_retry conda install --yes -c https://conda.binstar.org/kalefranz pysqlite
-travis_retry conda install --yes -c https://conda.binstar.org/jgomezdans libspatialite
+travis_retry pip install git+https://github.com/clarinova/pysqlite.git#egg=pysqlite
 
-git clone https://github.com/CivicKnowledge/ambry.git
-cd ambry
-git checkout develop
+travis_retry pip install -r requirements.txt
+travis_retry python setup.py develop
 
-python setup.py install
-
-ambry config install # Installs a development config
-
+ambry config install
