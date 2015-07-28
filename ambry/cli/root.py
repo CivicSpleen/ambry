22e20482686e69dc60e8c5af48e65dd3537d92f2
nmb10 <nmb10@dhcppc2.home>
2015-07-28 19:38:42 +0300

    makemigration command added. #91.

--- ambry/cli/root.py
+++ ambry/cli/root.py
@@ -20,61 +20,85 @@ def root_parser(cmd):
     sp = cmd.add_parser('list', help='List bundles and partitions')
     sp.set_defaults(command='root')
     sp.set_defaults(subcommand='list')
-    sp.add_argument('-P', '--plain', default=False, action="store_true", help="Print only vids")
+    sp.add_argument('-P', '--plain', default=False, action='store_true', help='Print only vids')
     sp.add_argument('-F', '--fields', type=str,
                     help="Specify fields to use. One of: 'locations', 'vid', 'status', 'vname', 'sname', 'fqname")
-    sp.add_argument('-p', '--partitions', default=False, action="store_true", help="Show partitions")
-    sp.add_argument('-t', '--tables', default=False, action="store_true", help="Show tables")
-    sp.add_argument('-a', '--all', default=False, action="store_true", help='List everything')
-    sp.add_argument('-l', '--library', default=False, action="store_const", const=lr.LIBRARY,
+    sp.add_argument('-p', '--partitions', default=False, action='store_true',
+                    help='Show partitions')
+    sp.add_argument('-t', '--tables', default=False, action='store_true',
+                    help='Show tables')
+    sp.add_argument('-a', '--all', default=False, action='store_true',
+                    help='List everything')
+    sp.add_argument('-l', '--library', default=False, action='store_const', const=lr.LIBRARY,
                     help='List only the library')
-    sp.add_argument('-r', '--remote', default=False, action="store_const", const=lr.REMOTE, help='List only the remote')
-    sp.add_argument('-s', '--source', default=False, action="store_const", const=lr.SOURCE, help='List only the source')
-    sp.add_argument('-w', '--warehouse', default=False, action="store_const", const='warehouse', help='List warehouses')
-    sp.add_argument('-c', '--collection', default=False, action="store_const", const='collection',
+    sp.add_argument('-r', '--remote', default=False, action='store_const', const=lr.REMOTE,
+                    help='List only the remote')
+    sp.add_argument('-s', '--source', default=False, action='store_const', const=lr.SOURCE,
+                    help='List only the source')
+    sp.add_argument('-w', '--warehouse', default=False, action='store_const', const='warehouse',
+                    help='List warehouses')
+    sp.add_argument('-c', '--collection', default=False, action='store_const', const='collection',
                     help='List collections')
-    sp.add_argument('term', nargs='?', type=str, help='Name or ID of the bundle or partition')
+    sp.add_argument('term', nargs='?', type=str,
+                    help='Name or ID of the bundle or partition')
+
+    sp = cmd.add_parser('makemigration', help='Create empty migration (for developers only).')
+    sp.set_defaults(command='root')
+    sp.set_defaults(subcommand='makemigration')
+    sp.add_argument('migration_name', type=str, help='Name of the migration')
 
     sp = cmd.add_parser('info', help='Information about a bundle or partition')
     sp.set_defaults(command='root')
     sp.set_defaults(subcommand='info')
     group = sp.add_mutually_exclusive_group()
-    group.add_argument('-l', '--library', default=False, action="store_true", help='Information about the library')
-    group.add_argument('-r', '--remote', default=False, action="store_true", help='Information about the remotes')
-    group.add_argument('-b', '--bundle', default=False, action="store_true", help='Information about a bundle')
-    sp.add_argument('term', type=str, nargs='?', help='Name or ID of the bundle or partition')
+    group.add_argument('-l', '--library', default=False, action='store_true',
+                       help='Information about the library')
+    group.add_argument('-r', '--remote', default=False, action='store_true',
+                       help='Information about the remotes')
+    group.add_argument('-b', '--bundle', default=False, action='store_true',
+                       help='Information about a bundle')
+    sp.add_argument('term', type=str, nargs='?',
+                    help='Name or ID of the bundle or partition')
 
     sp = cmd.add_parser('meta', help='Dump the metadata for a bundle')
     sp.set_defaults(command='root')
     sp.set_defaults(subcommand='meta')
-    sp.add_argument('term', type=str, nargs='?', help='Name or ID of the bundle or partition')
-    sp.add_argument('-k', '--key', default=False, type=str, help='Return the value of a specific key')
+    sp.add_argument('term', type=str, nargs='?',
+                    help='Name or ID of the bundle or partition')
+    sp.add_argument('-k', '--key', default=False, type=str,
+                    help='Return the value of a specific key')
     group = sp.add_mutually_exclusive_group()
-    group.add_argument('-y', '--yaml', default=False, action='store_true', help='Output yaml')
-    group.add_argument('-j', '--json', default=False, action='store_true', help='Output json')
-    group.add_argument('-r', '--rows', default=False, action='store_true', help='Output key/value pair rows')
-    sp.add_argument('terms', type=str, nargs=argparse.REMAINDER, help='Query commands to find packages with. ')
+    group.add_argument('-y', '--yaml', default=False, action='store_true',
+                       help='Output yaml')
+    group.add_argument('-j', '--json', default=False, action='store_true',
+                       help='Output json')
+    group.add_argument('-r', '--rows', default=False, action='store_true',
+                       help='Output key/value pair rows')
+    sp.add_argument('terms', type=str, nargs=argparse.REMAINDER,
+                    help='Query commands to find packages with. ')
 
     sp = cmd.add_parser('doc', help='Start the documentation server')
     sp.set_defaults(command='root')
     sp.set_defaults(subcommand='doc')
 
-    sp.add_argument('-c', '--clean', default=False, action="store_true",
+    sp.add_argument('-c', '--clean', default=False, action='store_true',
                     help='When used with --reindex, delete the index and old files first. ')
-    sp.add_argument('-d', '--debug', default=False, action="store_true", help='Debug mode ')
+    sp.add_argument('-d', '--debug', default=False, action='store_true', help='Debug mode ')
     sp.add_argument('-p', '--port', help='Run on a sepecific port, rather than pick a random one')
 
     sp = cmd.add_parser('search', help='Search the full-text index')
     sp.set_defaults(command='root')
     sp.set_defaults(subcommand='search')
     sp.add_argument('term', type=str, nargs=argparse.REMAINDER, help='Query term')
-    sp.add_argument('-l', '--list', default=False, action="store_true", help='List documents instead of search')
-    sp.add_argument('-i', '--identifiers', default=False, action="store_true", help='Search only the identifiers index')
-    sp.add_argument('-R', '--reindex', default=False, action="store_true",
+    sp.add_argument('-l', '--list', default=False, action='store_true',
+                    help='List documents instead of search')
+    sp.add_argument('-i', '--identifiers', default=False, action='store_true',
+                    help='Search only the identifiers index')
+    sp.add_argument('-R', '--reindex', default=False, action='store_true',
                     help='Generate documentation files and index the full-text search')
-    sp.add_argument('-d', '--document', default=False, action="store_true",
+    sp.add_argument('-d', '--document', default=False, action='store_true',
                     help='Return the search document for an object id')
-    sp.add_argument('-u', '--unparsed', default=False, action="store_true",
+    sp.add_argument('-u', '--unparsed', default=False, action='store_true',
                     help='Pass the search term to the engine without parsing')
 
     sp = cmd.add_parser('sync', help='Sync with the remotes')
@@ -96,30 +120,38 @@ def root_command(args, rc):
         l = new_library(rc)
         l.logger = global_logger
     except DatabaseError as e:
-        warn("No library: {}".format(e))
+        warn('No library: {}'.format(e))
         l = None
     except Exception as e:
-        warn("Failed to instantiate library: {}".format(e))
+        warn('Failed to instantiate library: {}'.format(e))
         l = None
 
     globals()['root_' + args.subcommand](args, l, rc)
 
 
+def root_makemigration(args, l, rc):
+    from ambry.orm.database import create_migration_template
+    file_name = create_migration_template(args.migration_name)
+    print 'New empty migration created. Now populate {} with appropriate sql.'.format(file_name)
+
+
 def root_list(args, l, rc):
 
-
-    ##
+    #
     # Listing warehouses and collections is different
+    #
 
     if args.collection:
-        #root_list_collections(args,l,rc)
+        # root_list_collections(args,l,rc)
         pass
 
     if args.warehouse:
-        #root_list_collections(args, l, rc)
+        # root_list_collections(args, l, rc)
         pass
-    ##
+
+    #
     # The remainder are for listing bundles and partitions.
+    #
 
     if args.tables:
         # root_list_tables(args, l, rc)
@@ -185,7 +217,7 @@ def root_info(args, l, rc):
     ident = l.resolve(args.term, location=locations)
 
     if not ident:
-        fatal("Failed to find record for: {}", args.term)
+        fatal('Failed to find record for: {}', args.term)
         return
 
     try:
@@ -209,13 +241,12 @@ def root_sync(args, l, config):
     """Sync with the remote. For more options, use library sync
     """
 
-    l.logger.info("==== Sync Remotes")
+    l.logger.info('==== Sync Remotes')
     l.sync_remotes()
 
 
 def root_search(args, l, config):
     # This will fetch the data, but the return values aren't quite right
-    from ambry.orm.exc import NotFoundError
 
     term = ' '.join(args.term)
 
@@ -301,7 +332,7 @@ def root_doc(args, l, rc):
 
     app_config['port'] = args.port if args.port else 8085
 
-    file_handler = FileHandler(os.path.join(cache_dir, "web.log"))
+    file_handler = FileHandler(os.path.join(cache_dir, 'web.log'))
     file_handler.setLevel(logging.WARNING)
     app.logger.addHandler(file_handler)
 
@@ -310,7 +341,7 @@ def root_doc(args, l, rc):
     if not args.debug:
         # Don't open the browser on debugging, or it will re-open on every
         # application reload
-        webbrowser.open("http://localhost:{}/".format(app_config['port']))
+        webbrowser.open('http://localhost:{}/'.format(app_config['port']))
 
     app.run(host=app_config['host'], port=int(app_config['port']), debug=args.debug)
 
--- doc/DEVEL-README.md
+++ doc/DEVEL-README.md
@@ -25,3 +25,13 @@ To run tests with coverage:
     2. Generage html:
       $ coverage html
     3. Open htmlcov/index.html in the browser.
+
+Changing models and database (AKA migration):
+    1. Change appropriate models.
+    2. Make new empty migration
+      $ ambry makemigration <your_migration_name>
+    3. Open file created by command and add appropriate sql to the _migrate_sqlite and _migrate_postgresql methods.
+    4. Set is_ready to True.
+    5. Increment orm.database.SCHEMA_VERSION (now your SCHEMA_VERSION has to match to migration number.) # FIXME: Do you really need SCHEMA_VERSION? If ambry sees missed migration, just migrate them.
+    6. Check just created migration on your own db.  (FIXME: Describe how.)
+    7. Commit and push file created by command.
