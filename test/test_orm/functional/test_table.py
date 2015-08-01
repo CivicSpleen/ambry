5830523dd8869376a5effe5b5838de868db1af24
nmb10 <nmb10@dhcppc2.home>
2015-08-01 19:14:50 +0300

    table tests fixed. #91.

--- test/test_orm/functional/test_table.py
+++ test/test_orm/functional/test_table.py
@@ -1,54 +1,37 @@
 
 import unittest
-import tempfile
-import uuid
-from ambry.orm import Column
-from ambry.orm import Partition
-from ambry.orm import Table
-from ambry.orm import Dataset
-from ambry.orm import Config
-from ambry.orm import File
-from ambry.orm import Code
-from ambry.orm import ColumnStat
-from sqlalchemy.orm import sessionmaker
-from ambry.identity import DatasetNumber, PartitionNumber
-from sqlalchemy.exc import IntegrityError
+from ambry.orm.database import Database
+from ambry.orm.dataset import Dataset
+from ambry.orm.partition import Partition
+from ambry.identity import DatasetNumber
+
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        from sqlalchemy import create_engine
 
-        super(Test,self).setUp()
+        super(Test, self).setUp()
 
         self.dsn = 'sqlite://'
 
         # Make an array of dataset numbers, so we can refer to them with a single integer
         self.dn = [str(DatasetNumber(x, x)) for x in range(1, 10)]
 
-
-
     def new_dataset(self, n):
-        return Dataset(vid=self.dn[n], source='source', dataset='dataset' )
+        return Dataset(vid=self.dn[n], source='source', dataset='dataset')
 
-
-
-    def new_partition(self,ds,n):
+    def new_partition(self, ds, n):
 
         t_vids = sorted(t.id_ for t in ds.tables)
 
         return Partition(ds, sequence_id=n, t_id=t_vids[n])
 
     def dump_database(self, db, table):
-        import sys
-        from subprocess import check_output
-
         for row in db.connection.execute("SELECT * FROM {}".format(table)):
             print row
 
     def test_table_basic(self):
         """Basic operations on datasets"""
-        from ambry.orm.database import Database
 
         db = Database(self.dsn)
         db.open()
@@ -63,11 +46,5 @@ class Test(unittest.TestCase):
         t1.add_column('col1', description='foobar')
 
         db.commit()
-
-        self.dump_database(db,'columns')
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(Test))
-    return suite
-
+        # uncomment to see database content.
+        # self.dump_database(db, 'columns')
