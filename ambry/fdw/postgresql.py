8aa52d9cc7d2b74287a72a291700b217b15ae6dd
nmb10 <nmb10@dhcppc2.home>
2015-08-28 13:42:23 +0300

    Sqlalchemy FDW implemented. #99.

--- ambry/fdw/postgresql.py
+++ ambry/fdw/postgresql.py
@@ -1,11 +1,53 @@
 # -*- coding: utf-8 -*-
 import logging
 
+from sqlalchemy import Table, MetaData
+from sqlalchemy.orm import mapper
 from sqlalchemy.sql.expression import text
 
 from ambry.util import get_logger
 
 logger = get_logger(__name__, level=logging.DEBUG, propagate=False)
+
+
+def add_partition(connection, partition):
+    FOREIGN_SERVER_NAME = 'partition_server'
+    _create_if_not_exists(connection, FOREIGN_SERVER_NAME)
+
+    columns = ['{} {}'.format(c.name, c.type.compile()) for c in partition.table.columns]
+    query = """
+        CREATE FOREIGN TABLE {table_name} (
+            {columns}
+        ) server {server_name} options (
+            filename '{file_name}'
+        );
+    """.format(table_name=_table_name(partition), columns=',\n'.join(columns),
+               server_name=FOREIGN_SERVER_NAME, file_name=partition.datafile.syspath)
+    logging.debug('Create foreign table for {} partition. Query:\n{}.'.format(partition.vid, query))
+    connection.execute(query)
+
+
+def _as_orm(connection, partition):
+    """ Returns sqlalchemy table for partition rows.
+
+    Example:
+        PartitionRow = _as_orm(connection, partition)
+        print session.query(PartitionRow).all()
+
+    Returns:
+        FIXME:
+    """
+
+    # FIXME: That solution is not documented by multicorn. Try documented solution again.
+
+    class PartitionRow(object):
+        pass
+
+    table_name = _table_name(partition)
+    metadata = MetaData(bind=connection.engine)
+    t = Table(table_name, metadata, *partition.table.columns)
+    mapper(PartitionRow, t)
+    return PartitionRow
 
 
 def _server_exists(connection, server_name):
@@ -36,20 +78,3 @@ def _table_name(partition):
     # p_{vid}_ft stands for partition_vid_foreign_table
     # FIXME: it seems prefix + partition.table.name is better choice for foreign table name.
     return 'p_{vid}_ft'.format(vid=partition.vid)
-
-
-def add_partition(connection, partition):
-    FOREIGN_SERVER_NAME = 'partition_server'
-    _create_if_not_exists(connection, FOREIGN_SERVER_NAME)
-
-    columns = ['{} {}'.format(c.name, c.type.compile()) for c in partition.table.columns]
-    query = """
-        CREATE FOREIGN TABLE {table_name} (
-            {columns}
-        ) server {server_name} options (
-            filename '{file_name}'
-        );
-    """.format(table_name=_table_name(partition), columns=',\n'.join(columns),
-               server_name=FOREIGN_SERVER_NAME, file_name=partition.datafile.syspath)
-    logging.debug('Create foreign table for {} partition. Query:\n{}.'.format(partition.vid, query))
-    connection.execute(query)
