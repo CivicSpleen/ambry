8621e837c8e963e4b1c9448bdc8f0be413eba4ce
Eric Busboom <eric@busboom.org>
2015-11-24 09:16:50 -0700

    Getting tests to work again

---
+++ test/functional/test_bundles.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from test.test_base import TestBase
+
+
+class Test(TestBase):
+
+    def test_headerstypes(self):
+        import unicodecsv as csv
+
+        b = self.import_single_bundle('ingest.example.com/headerstypes')
+        b.run()
+
+    def test_basic(self):
+        b = self.import_single_bundle('ingest.example.com/basic')
+        b.run()
+
+    def test_stages(self):
+        b = self.import_single_bundle('ingest.example.com/stages')
+        b.run()
+
+    def test_casters(self):
+        b = self.import_single_bundle('build.example.com/casters')
+        b.run()
