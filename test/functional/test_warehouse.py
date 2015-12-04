cca7575ab23de411a167d7811f03aa4b366154ee
nmb10 <nmb10@dhcppc2.home>
2015-12-04 18:41:28 +0300

    Tests refactored. #139.

--- test/functional/test_warehouse.py
+++ test/functional/test_warehouse.py
@@ -27,22 +27,6 @@ class Mixin(object):
         ''' Raises AssertionError if column is not indexed. '''
         raise NotImplementedError('Override the method and provide db specific index check.')
 
-    def _get_generator_source(self, rows=None):
-        if not rows:
-            rows = [
-                [0, 0],
-                [1, 1],
-                [2, 2]]
-
-        def gen(rows=rows):
-            # generate header
-            yield ['col1', 'col2']
-
-            # generate some rows
-            for row in rows:
-                yield row
-        return GeneratorSource(SourceSpec('foobar'), gen())
-
     def test_query_mpr_with_auto_install(self):
         if isinstance(self, PostgreSQLTest):
             if Version(AMBRY_SOURCES_VERSION) < Version('0.1.6'):
@@ -58,16 +42,14 @@ class Mixin(object):
         bundle.wrap_partition(partition1)
 
         try:
-            datafile = MPRowsFile(bundle.build_fs, partition1.cache_key)
-            datafile.load_rows(self._get_generator_source())
-            partition1._datafile = datafile
+            partition1._datafile = _get_datafile(bundle.build_fs, partition1.cache_key)
             rows = library.warehouse.query('SELECT * FROM {};'.format(partition1.vid))
             self.assertEqual(rows, [(0, 0), (1, 1), (2, 2)])
         finally:
             library.warehouse.close()
             library.database.close()
 
-    def test_install_and_query_materialized_table(self):
+    def test_install_and_query_materialized_partition(self):
         # materialized view for postgres and readonly table for sqlite.
         if isinstance(self, PostgreSQLTest):
             _assert_valid_ambry_sources()
@@ -82,9 +64,7 @@ class Mixin(object):
         bundle.wrap_partition(partition1)
 
         try:
-            datafile = MPRowsFile(bundle.build_fs, partition1.cache_key)
-            datafile.load_rows(self._get_generator_source())
-            partition1._datafile = datafile
+            partition1._datafile = _get_datafile(bundle.build_fs, partition1.cache_key)
 
             # materialize partition (materialized view for postgres, readonly table for sqlite)
             library.warehouse.materialize(partition1.vid)
@@ -94,7 +74,7 @@ class Mixin(object):
 
             # now drop the *.mpr file and check again. Query should return the same data.
             #
-            syspath = datafile.syspath
+            syspath = partition1._datafile.syspath
             os.remove(syspath)
             self.assertFalse(os.path.exists(syspath))
             rows = library.warehouse.query('SELECT * FROM {};'.format(partition1.vid))
@@ -117,9 +97,7 @@ class Mixin(object):
         bundle.wrap_partition(partition1)
 
         try:
-            datafile = MPRowsFile(bundle.build_fs, partition1.cache_key)
-            datafile.load_rows(self._get_generator_source())
-            partition1._datafile = datafile
+            partition1._datafile = _get_datafile(bundle.build_fs, partition1.cache_key)
 
             # Index creation requires materialized tables.
             library.warehouse.materialize(partition1.vid)
@@ -139,7 +117,7 @@ class Mixin(object):
             # FIXME: Use library.warehouse.close() only.
             library.database.close()
 
-    def test_table_query(self):
+    def test_table_install_and_query(self):
         if isinstance(self, PostgreSQLTest):
             _assert_valid_ambry_sources()
         else:
@@ -162,13 +140,8 @@ class Mixin(object):
         bundle.wrap_partition(partition2)
 
         try:
-            datafile1 = MPRowsFile(bundle.build_fs, partition1.cache_key)
-            datafile1.load_rows(self._get_generator_source())
-            partition1._datafile = datafile1
-
-            datafile2 = MPRowsFile(bundle.build_fs, partition2.cache_key)
-            datafile2.load_rows(self._get_generator_source(rows=[[3, 3], [4, 4]]))
-            partition2._datafile = datafile2
+            partition1._datafile = _get_datafile(bundle.build_fs, partition1.cache_key)
+            partition2._datafile = _get_datafile(bundle.build_fs, partition2.cache_key, rows=[[3, 3], [4, 4]])
 
             # Install table
             library.warehouse.install(table1.vid)
@@ -340,3 +313,26 @@ def _assert_sqlite_index(warehouse, partition, column):
     query = 'EXPLAIN QUERY PLAN SELECT * FROM {} WHERE {} > 1;'.format(table, column)
     result = cursor.execute(query).fetchall()
     assert 'USING INDEX' in result[0][-1]
+
+
+def _get_generator_source(rows=None):
+    if not rows:
+        rows = [
+            [0, 0],
+            [1, 1],
+            [2, 2]]
+
+    def gen(rows=rows):
+        # generate header
+        yield ['col1', 'col2']
+
+        # generate some rows
+        for row in rows:
+            yield row
+    return GeneratorSource(SourceSpec('foobar'), gen())
+
+
+def _get_datafile(fs, path, rows=None):
+    datafile = MPRowsFile(fs, path)
+    datafile.load_rows(_get_generator_source(rows=rows))
+    return datafile
