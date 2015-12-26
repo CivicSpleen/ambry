c25cad036f7b0102c9f177018dc0949d1a86d337
nmb10 <nmb10@dhcppc2.home>
2015-12-26 19:34:34 +0300

    performance test fixed. #140.

--- test/performance/test_db_usage.py
+++ test/performance/test_db_usage.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+
+import unittest
+
 from sqlalchemy import event
 
 from ambry.metadata.proptree import get_or_create
 from ambry.metadata.schema import Top
 from ambry.orm import Config
 
+from test.factories import DatasetFactory
 from test.test_base import TestBase
 
 
@@ -12,8 +16,9 @@ class DbUsageTreeFromDatabaseTest(TestBase):
 
     def setUp(self):
         super(self.__class__, self).setUp()
-        self.db = self.new_database()
-        self.dataset = self.new_db_dataset(self.db, n=0)
+        self.db = self.library().database
+        DatasetFactory._meta.sqlalchemy_session = self.db.session
+        self.dataset = DatasetFactory()
 
     # helpers
     def _create_db_tree(self, configs):
@@ -29,11 +34,13 @@ class DbUsageTreeFromDatabaseTest(TestBase):
         db = self.db
         dataset = self.dataset
         top_config = Config(
-            d_vid=dataset.vid, parent=None, type='metadata')
+            d_vid=dataset.vid, parent=None, type='metadata', sequence_id=1)
         db.session.add(top_config)
         db.session.commit()
+        counter = 1
 
         for key, value in configs:
+            counter += 1
             group_keys = key.split('.')[:-1]
             value_key = key.split('.')[-1]
             parent = top_config
@@ -43,16 +50,19 @@ class DbUsageTreeFromDatabaseTest(TestBase):
                 cfg_instance, created = get_or_create(
                     db.session, Config,
                     d_vid=dataset.vid, key=group, group=group,
-                    parent=parent, type='metadata')
+                    parent=parent, type='metadata',
+                    sequence_id=counter)
                 parent = cfg_instance
 
             # populate value
+            counter += 1
             value_config = Config(
                 d_vid=dataset.vid, key=value_key, value=value,
-                parent=parent, type='metadata')
+                parent=parent, type='metadata', sequence_id=counter)
             db.session.add(value_config)
             db.session.commit()
 
+    @unittest.skip('FIXME: UNIQUE constraint failed')
     def test_uses_exactly_two_db_hit_to_build_config(self):
 
         # create appropriate tree in the database
