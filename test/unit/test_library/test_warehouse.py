ac4cc86bf75fb7e8b0948d0c80887a5e7434e0ab
nmb10 <nmb10@dhcppc2.home>
2016-02-02 17:39:16 +0300

    Warehouse broken unit tests fixed. #189.

--- test/unit/test_library/test_warehouse.py
+++ test/unit/test_library/test_warehouse.py
@@ -1,51 +1,39 @@
 # -*- coding: utf-8 -*-
+from unittest import TestCase
 
+from ambry.library import Library
 from ambry.library.warehouse import Warehouse
 
-from test.factories import PartitionFactory
-from test.test_base import TestBase
+from ambry.orm import Partition
 
 try:
     # py2, mock is external lib.
-    from mock import patch
+    from mock import patch, MagicMock
 except ImportError:
     # py3, mock is included
-    from unittest.mock import patch
+    from unittest.mock import patch, MagicMock
 
 
-class WarehouseTest(TestBase):
+class WarehouseTest(TestCase):
     def setUp(self):
         super(self.__class__, self).setUp()
-        self.my_library = self.library()
+        self._my_library = MagicMock(spec=Library)
 
     # __init__ tests
     def test_uses_library_db(self):
-        config_warehouse = self.my_library.config.library.warehouse
-        try:
-            self.my_library.config.library.warehouse = None
-            self.warehouse = Warehouse(self.my_library)
-            self.assertEqual(self.my_library.database.dsn, self.warehouse._backend._dsn)
-        finally:
-            # restore value from config
-            self.my_library.config.library.warehouse = config_warehouse
+        self._my_library.config.library.warehouse = None
+        warehouse = Warehouse(self._my_library)
+        self.assertEqual(self._my_library.database.dsn, warehouse._backend._dsn)
 
-    def test_uses_database_from_config(self):
-        config_warehouse = self.my_library.config.library.warehouse
-        try:
-            self.my_library.config.library.warehouse = 'sqlite:////tmp/temp1.db'
-            self.warehouse = Warehouse(self.my_library)
-            self.assertEqual('sqlite:////tmp/temp1.db', self.warehouse._backend._dsn)
-        finally:
-            # restore value from config
-            self.my_library.config.library.warehouse = config_warehouse
+    def test_uses_given_dsn(self):
+        warehouse = Warehouse(self._my_library, dsn='sqlite:////tmp/temp1.db')
+        self.assertEqual('sqlite:////tmp/temp1.db', warehouse._backend._dsn)
 
     # .query() tests
     def test_sends_query_to_database_backend(self):
-        w = Warehouse(self.my_library)
-        PartitionFactory._meta.sqlalchemy_session = self.my_library.database.session
-        partition = PartitionFactory()
+        w = Warehouse(self._my_library, dsn='sqlite:////tmp/temp1.db')
         with patch.object(w._backend, 'query') as fake_query:
-            query = 'SELECT * FROM {};'.format(partition.vid)
+            query = 'SELECT * FROM p1vid;'
             w.query(query)
 
             # backend.query called once.
@@ -56,15 +44,19 @@ class WarehouseTest(TestBase):
 
     # .install() tests
     def test_finds_partition_by_refs_and_installs_partition_to_backend(self):
-        w = Warehouse(self.my_library)
-        PartitionFactory._meta.sqlalchemy_session = self.my_library.database.session
-        partition = PartitionFactory()
+        w = Warehouse(self._my_library, dsn='sqlite:////tmp/temp1.db')
+        fake_partition = MagicMock(spec=Partition)
+        fake_partition.vid = 'p1vid'
+        fake_partition.ref = 'p1vid'
+
+        # Make library to return my partition as search result.
+        self._my_library.partition.return_value = fake_partition
 
         with patch.object(w._backend, 'install') as fake_install:
-            w.install(partition.vid)
+            w.install(fake_partition.vid)
 
             # backend.query called once.
             self.assertEqual(len(fake_install.mock_calls), 1)
 
             # second argument of the is found partition.
-            self.assertEqual(fake_install.mock_calls[0][1][1].ref, partition.ref)
+            self.assertEqual(fake_install.mock_calls[0][1][1].ref, fake_partition.ref)
