69605844667c67eda1fdd24807a67395e3dd0659
nmb10 <nmb10@dhcppc2.home>
2016-02-12 03:45:34 +0300

    Regression tests use proto library. #187.

--- test/regression/test_29_ambry_sources.py
+++ test/regression/test_29_ambry_sources.py
@@ -1,39 +1,20 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
 
-from test.test_base import TestBase
+from test.proto import TestBase
 
-# This test produces this error:
-#
-# Traceback (most recent call last):
-#   File "/Users/eric/proj/virt/ambry-develop/ambry/test/regression/test_29_ambry_sources.py", line 22, in test_bundle_warehouse
-#     print(wh.materialize('build.example.com-casters-simple'))
-#   File "/Users/eric/proj/virt/ambry-develop/ambry/ambry/library/warehouse.py", line 129, in materialize
-#     return self._backend.install(connection, partition, materialize=True)
-#   File "/Users/eric/proj/virt/ambry-develop/ambry/ambry/mprlib/backends/sqlite.py", line 39, in install
-#     self._add_partition(connection, partition)
-#   File "/Users/eric/proj/virt/ambry-develop/ambry/ambry/mprlib/backends/sqlite.py", line 271, in _add_partition
-#     sqlite_med.add_partition(connection, partition.datafile, partition.vid)
-#   File "/Users/eric/proj/virt/ambry-develop/ambry_sources/ambry_sources/med/sqlite.py", line 135, in add_partition
-#     cursor.execute(query)
-#   File "src/vtable.c", line 175, in VirtualTable.xCreate.sqlite3_declare_vtab
-# SQLError: SQLError: near "index": syntax error
 
-class BundleWarehouse(TestBase):
-
+class Test(TestBase):
 
     def test_index_column_fails(self):
+        if self._db_type != 'sqlite':
+            self.skipTest('SQLite tests are disabled.')
 
-        l = self.library()
+        bundle = self.import_single_bundle('build.example.com/generators')
+        bundle.ingest()
+        bundle.source_schema()
+        bundle.schema()
+        bundle.build()
 
-        b = l.bundle('build.example.com-casters')
-
-        wh = b.warehouse('test')
-
+        wh = bundle.warehouse('test')
         wh.clean()
-
-        print(wh.dsn)
-
-        print(wh.materialize('build.example.com-casters-simple'))
-
-
+        self.assertIsNotNone(wh.materialize('build.example.com-generators-demo'))
