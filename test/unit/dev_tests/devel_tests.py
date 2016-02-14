064d20057b04968a00de5d2d74f8c3edb6eb383b
nmb10 <nmb10@dhcppc2.home>
2016-02-14 11:50:19 +0300

    Devel tests use proto library. #187.

--- test/unit/dev_tests/devel_tests.py
+++ test/unit/dev_tests/devel_tests.py
@@ -5,49 +5,15 @@ import unittest
 
 from six import u
 
-from test.test_base import TestBase
-from six.moves.urllib.parse import urlparse
+from test.proto import TestBase
 
 
 class Test(TestBase):
 
     @unittest.skip('Development Test')
-    def test_install(self):
-        """Test copying a bundle to a remote, then streaming it back"""
-        from boto.exception import S3ResponseError
-        b = self.setup_bundle('simple', source_url='temp://')
-        l = b._library
-
-        b.sync_in()
-
-        b.run()
-
-        self.assertEqual(1, len(list(l.bundles)))
-
-        p = list(b.partitions)[0]
-        p_vid = p.vid
-
-        self.assertEqual(497054, int(sum(row[3] for row in p.stream(skip_header=True))))
-
-        self.assertEqual('build', l.partition(p_vid).location)
-
-        try:
-            remote_name, path = b.checkin()
-        except S3ResponseError as exc:
-            if exc.status == 403:  # Forbidden.
-                raise unittest.SkipTest(
-                    'Skip S3 error - {}. It seems S3 credentials are not valid.'.format(exc))
-            else:
-                raise
-
-        print(remote_name, path)
-
-    @unittest.skip('Development Test')
     def test_search(self):
         """Test copying a bundle to a remote, then streaming it back"""
-        from ambry.library import new_library
-
-        l = new_library(self.get_rc())
+        l = self.library()
 
         l.sync_remote('test')
 
@@ -73,23 +39,3 @@ class Test(TestBase):
         print(search.search_datasets('Example')[0].vid)
 
         print(search.search_datasets('2010'))
-
-    @unittest.skip('Development Test')
-    def test_sequence(self):
-        from ambry.orm import Database
-
-        conf = self.get_rc()
-
-        if 'database' in conf.dict and 'postgresql-test' in conf.dict['database']:
-            dsn = conf.dict['database']['postgresql-test']
-            parsed_url = urlparse(dsn)
-            db_name = parsed_url.path.replace('/', '')
-            self.postgres_dsn = parsed_url._replace(path='postgres').geturl()
-            self.postgres_test_db = '{}_test_db1ae'.format(db_name)
-            self.postgres_test_dsn = parsed_url._replace(path=self.postgres_test_db).geturl()
-
-        db = Database(self.postgres_test_dsn)
-
-        for i in range(10):
-
-            print(b.dataset.next_number('foobar'))
