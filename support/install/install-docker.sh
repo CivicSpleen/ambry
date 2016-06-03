2ed59f465c2ac8453e10bcee6a1f29f8e59ba558
Eric Busboom <eric@clarinova.com>
2016-06-03 13:41:04 -0700

    .

--- support/install/install-docker.sh
+++ support/install/install-docker.sh
@@ -6,9 +6,15 @@
 #
 # Note, this installs Ambry in a running docker container; it does not create a docker image.
 
+container_name=${1:-'ambry'}
+
 image=ubuntu:16.04
 
 this_dir="$(cd "$(dirname "$0")"; pwd)"
 root_dir="$( dirname $(dirname $this_dir))"
 
-docker run -ti -v $root_dir:/tmp/ambry $image /bin/bash -c "/bin/bash /tmp/ambry/support/install/install-ubuntu.sh; /bin/bash"
+echo $this_dir
+echo $root_dir
+
+echo docker run -ti --name $container_name -v $root_dir:/tmp/ambry $image \
+    -c "/bin/bash /tmp/ambry/support/install/install-ubuntu.sh; /bin/bash"
